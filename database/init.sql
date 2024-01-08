03858830a818113a908e66da0289c05d2e6ab081
jl3nn <jonas.lennie.21@ucl.ac.uk>
2024-01-08 17:48:26 +0000

    starting work on db

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,41 @@
-CREATE TABLE #bla (
-    # bla
+CREATE DATABASE comp0022;
+
+CREATE TABLE movies (
+    movieId INT,
+    title VARCHAR(255),
+    genres VARCHAR(255),
+    PRIMARY KEY (movieId)
 );
 
-COPY bla FROM '/docker-entrypoint-initdb.d/data.csv' WITH CSV HEADER;
+CREATE TABLE ratings (
+    userId INT,
+    movieId INT,
+    rating FLOAT,
+    timestamp INT,
+    PRIMARY KEY (userId, movieId),
+    FOREIGN KEY (userId) REFERENCES movies(userId),
+    FOREIGN KEY (movieId) REFERENCES movies(movieId)
+);
+
+CREATE TABLE tags (
+    userId INT,
+    movieId INT,
+    tag VARCHAR(255),
+    timestamp INT,
+    PRIMARY KEY (userId, movieId),
+    FOREIGN KEY (userId) REFERENCES movies(userId),
+    FOREIGN KEY (movieId) REFERENCES movies(movieId)
+);
+
+CREATE TABLE links (
+    movieId INT,
+    imdbId VARCHAR(20),
+    tmdbId INT,
+    PRIMARY KEY (movieId),
+    FOREIGN KEY (movieId) REFERENCES movies(movieId)
+);
+
+COPY ratings FROM '/docker-entrypoint-initdb.d/ratings.csv' DELIMITER ',' CSV HEADER;
+COPY tags FROM '/docker-entrypoint-initdb.d/tags.csv' DELIMITER ',' CSV HEADER;
+COPY movies FROM '/docker-entrypoint-initdb.d/movies.csv' DELIMITER ',' CSV HEADER;
+COPY links FROM '/docker-entrypoint-initdb.d/links.csv' DELIMITER ',' CSV HEADER;
--- server/server.py
+++ server/server.py
@@ -6,20 +6,53 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-DATABASE_URL = "postgresql://admin:top_secret_password@localhost:5432/comp0022"
+db_params = {
+    'dbname': 'comp0022',
+    'user': 'admin',
+    'password': 'top_secret_password',
+    'host': 'localhost',
+    'port': '5432'
+}
 
-def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)
+def execute_query(query):
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+    return result
 
 @app.route('/')
-@cross_origin()
 def hello_world():
-    query = "SELECT * FROM bla LIMIT 25;"
-    with get_db_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(query)
-            rows = cursor.fetchall()
-    return jsonify(rows)
+    return 'Hello, World!'
+
+@app.route('/ratings')
+def get_ratings():
+    query = "SELECT * FROM ratings LIMIT 5;"
+    result = execute_query(query)
+    result = [{'userId': row[0], 'movieId': row[1], 'rating': row[2], 'timestamp': row[3]} for row in result]
+    return jsonify(result)
+
+@app.route('/tags')
+def get_tags():
+    query = "SELECT * FROM tags LIMIT 5;"
+    result = execute_query(query)
+    result = [{'userId': row[0], 'movieId': row[1], 'tag': row[2], 'timestamp': row[3]} for row in result]
+    return jsonify(result)
+
+@app.route('/movies')
+def get_movies():
+    query = "SELECT * FROM movies LIMIT 5;"
+    result = execute_query(query)
+    result = [{'movieId': row[0], 'title': row[1], 'genres': row[2]} for row in result]
+    return jsonify(result)
+
+@app.route('/links')
+def get_links():
+    query = "SELECT * FROM links LIMIT 5;"
+    result = execute_query(query)
+    result = [{'movieId': row[0], 'imdbId': row[1], 'tmdbId': row[2]} for row in result]
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555, host='0.0.0.0')
