7f23dd18d100a4353961c345bd6cd010598c1c6e
jl3nn <jonas.lennie.21@ucl.ac.uk>
2024-01-10 01:17:38 +0000

    adding personality data

--- database/init.sql
+++ database/init.sql
@@ -27,6 +27,28 @@ CREATE TABLE tags (
     FOREIGN KEY(movieId) REFERENCES movies(movieId)
 );
 
+CREATE TABLE users (
+    userid VARCHAR(255),
+    openness FLOAT,
+    agreeableness FLOAT,
+    emotional_stability FLOAT,
+    conscientiousness FLOAT,
+    extraversion FLOAT,
+    assigned_metric VARCHAR(50),
+    assigned_condition VARCHAR(50),
+    is_personalized FLOAT,
+    enjoy_watching FLOAT
+);
+
+CREATE TABLE predicted_ratings (
+    userid VARCHAR(255),
+    movie INT,
+    predicted_rating FLOAT
+);
+
+
 COPY movies (movieId, title, genres, imdbId, tmdbId, imageUrl) FROM '/docker-entrypoint-initdb.d/movies.csv' DELIMITER ',' CSV HEADER;
 COPY ratings (userId, movieId, rating, timestamp) FROM '/docker-entrypoint-initdb.d/ratings.csv' DELIMITER ',' CSV HEADER;
 COPY tags (userId, movieId, tag, timestamp) FROM '/docker-entrypoint-initdb.d/tags.csv' DELIMITER ',' CSV HEADER;
+COPY users (userid, openness, agreeableness, emotional_stability, conscientiousness, extraversion, assigned_metric, assigned_condition, is_personalized, enjoy_watching) FROM '/docker-entrypoint-initdb.d/users.csv' DELIMITER ',' CSV HEADER;
+COPY predicted_ratings (userid, movie, predicted_rating) FROM '/docker-entrypoint-initdb.d/predicted_ratings.csv' DELIMITER ',' CSV HEADER;
--- database/preprocessing.py
+++ database/preprocessing.py
@@ -1,24 +1,44 @@
 import pandas as pd
 import os
 
-processed_dir = 'processed/'
-if not os.path.exists(processed_dir):
-    os.makedirs(processed_dir)
+processed_data_dir = 'processed/'
+if not os.path.exists(processed_data_dir):
+    os.makedirs(processed_data_dir)
 
 # 1. Normalise the data by merging movies with their links on the 'movieId' column
 movies_df = pd.read_csv(f"movies.csv")
 links_df = pd.read_csv(f"links.csv", dtype={'tmdbId': 'str'})
 
-merged_df = pd.merge(movies_df, links_df, on='movieId')
+merged_df = pd.merge(movies_df, links_df, on='movieId', how='left')
 
-merged_df.to_csv(f'{processed_dir}movies.csv', index=False)
+# 2. Add movie image links 
+images_df = pd.read_csv(f"image_assets.csv")
 
-# 2. Convert the unix timestamp to a datetime object
+merged_df = pd.merge(merged_df, images_df, left_on='movieId', right_on='item_id', how='left')
+merged_df.rename(columns={'image': 'imageUrl'}, inplace=True)
+merged_df.drop(columns=['item_id'], inplace=True)
+
+merged_df.to_csv(f'{processed_data_dir}movies.csv', index=False)
+
+# 3. Convert the unix timestamp to a datetime object
 ratings_df = pd.read_csv(f"ratings.csv")
 tags_df = pd.read_csv(f"tags.csv")
 
 ratings_df['timestamp'] = pd.to_datetime(ratings_df['timestamp'], unit='s')
 tags_df['timestamp'] = pd.to_datetime(tags_df['timestamp'], unit='s')
 
-ratings_df.to_csv(f'{processed_dir}ratings.csv', index=False)
-tags_df.to_csv(f'{processed_dir}tags.csv', index=False)
+ratings_df.to_csv(f'{processed_data_dir}ratings.csv', index=False)
+tags_df.to_csv(f'{processed_data_dir}tags.csv', index=False)
+
+# 4. Create a new table with the predicted rating for each movie
+original_data = pd.read_csv('personality-data.csv', delimiter=', ', engine='python')
+users_data = original_data[['userid', 'openness', 'agreeableness', 'emotional_stability',
+                             'conscientiousness', 'extraversion', 'assigned_metric',
+                             'assigned_condition', 'is_personalized', 'enjoy_watching']].copy()
+users_data.to_csv(f'{processed_data_dir}users.csv', index=False)
+
+dfs = []
+for i in range(1, 13):
+    dfs.append(original_data[['userid', f'movie_{i}', f'predicted_rating_{i}']].rename(columns={f'movie_{i}': 'movie', f'predicted_rating_{i}': 'predicted_rating'}))
+predicted_ratings_df = pd.concat(dfs, ignore_index=True)
+predicted_ratings_df.to_csv(f'{processed_data_dir}predicted_ratings.csv', index=False)
