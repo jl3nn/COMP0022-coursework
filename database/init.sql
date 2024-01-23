3c81b43b61bb933d8a74a40d07e353502b893bf1
Michael Berlingieri <47005505+miberl@users.noreply.github.com>
2024-01-23 19:15:18 +0000

    Fix duplicate primary key issue (#21)

--- database/init.sql
+++ database/init.sql
@@ -8,21 +8,6 @@ CREATE TABLE movies (
     PRIMARY KEY (movie_id)
 );
 
-CREATE TABLE users (
-    user_id VARCHAR(255),
-    openness FLOAT,
-    agreeableness FLOAT,
-    emotional_stability FLOAT,
-    conscientiousness FLOAT,
-    extraversion FLOAT,
-    assigned_metric VARCHAR(50),
-    assigned_condition VARCHAR(50),
-    is_personalized INT,
-    enjoy_watching INT, 
-    PRIMARY KEY(user_id)
-);
-
-
 CREATE TABLE ratings (
     user_id INT,
     movie_id INT,
@@ -45,6 +30,21 @@ CREATE TABLE tags (
 /*
 Note: for now we are keeping this separate from the main ratings table, but we may want to merge them later
 */ 
+
+CREATE TABLE users (
+    user_id VARCHAR(255),
+    openness FLOAT,
+    agreeableness FLOAT,
+    emotional_stability FLOAT,
+    conscientiousness FLOAT,
+    extraversion FLOAT,
+    assigned_metric VARCHAR(50),
+    assigned_condition VARCHAR(50),
+    is_personalized INT,
+    enjoy_watching INT, 
+    PRIMARY KEY(user_id)
+);
+
 CREATE TABLE ratings_personality(
     user_id VARCHAR(255),
     movie_id INT,
@@ -65,8 +65,5 @@ CREATE TABLE predicted_ratings (
 
 
 COPY movies (movie_id, title, genres, imdb_id, tmdb_id, image_url) FROM '/docker-entrypoint-initdb.d/movies.csv' DELIMITER ',' CSV HEADER;
-COPY users (user_id, openness, agreeableness, emotional_stability, conscientiousness, extraversion, assigned_metric, assigned_condition, is_personalized, enjoy_watching) FROM '/docker-entrypoint-initdb.d/users.csv' DELIMITER ',' CSV HEADER;
 COPY ratings (user_id, movie_id, rating, timestamp) FROM '/docker-entrypoint-initdb.d/ratings.csv' DELIMITER ',' CSV HEADER;
-COPY ratings_personality (user_id, movie_id, rating, timestamp) FROM '/docker-entrypoint-initdb.d/ratings_personality.csv' DELIMITER ',' CSV HEADER;
 COPY tags (user_id, movie_id, tag, timestamp) FROM '/docker-entrypoint-initdb.d/tags.csv' DELIMITER ',' CSV HEADER;
-COPY predicted_ratings (user_id, movie_id, predicted_rating) FROM '/docker-entrypoint-initdb.d/predicted_ratings.csv' DELIMITER ',' CSV HEADER;
