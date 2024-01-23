8a09616df77d31f8ffe5d9ad5b8888b3e7a45024
jl3nn <jonas.lennie.21@ucl.ac.uk>
2024-01-23 23:32:30 +0000

    Fixing some small bugs with new merge

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import blueprints
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin 
-import psycopg2
+import psycopg
 from flask import request
 from prometheus_flask_exporter import PrometheusMetrics
 
