c1f1f23544ee2d7954abbece4098241d3771796d
miberl <m.berlingieri02@gmail.com>
2024-01-25 17:51:23 +0000

    Added genres, users to init.sql

--- database/init.sql
+++ database/init.sql
@@ -8,6 +8,11 @@ CREATE TABLE movies (
     PRIMARY KEY (movie_id)
 );
 
+CREATE TABLE users (
+    user_id INT,
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE ratings (
     user_id INT,
     movie_id INT,
@@ -27,43 +32,24 @@ CREATE TABLE tags (
     FOREIGN KEY(movie_id) REFERENCES movies(movie_id)
 );
 
-/*
-Note: for now we are keeping this separate from the main ratings table, but we may want to merge them later
-*/ 
-
-CREATE TABLE users (
-    user_id VARCHAR(255),
-    openness FLOAT,
-    agreeableness FLOAT,
-    emotional_stability FLOAT,
-    conscientiousness FLOAT,
-    extraversion FLOAT,
-    assigned_metric VARCHAR(50),
-    assigned_condition VARCHAR(50),
-    is_personalized INT,
-    enjoy_watching INT, 
-    PRIMARY KEY(user_id)
+CREATE TABLE genres (
+    genre_id INT,
+    genre VARCHAR(255) NOT NULL,
+    PRIMARY KEY(genre_id)
 );
 
-CREATE TABLE ratings_personality(
-    user_id VARCHAR(255),
-    movie_id INT,
-    rating FLOAT NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id), 
-    FOREIGN KEY(user_id) REFERENCES users(user_id)
-);
-
-CREATE TABLE predicted_ratings (
-    user_id VARCHAR(255),
-    movie_id INT,
-    predicted_rating FLOAT, 
-    PRIMARY KEY(user_id, movie_id),
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id)
+CREATE TABLE movies_genres (
+    movie_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    PRIMARY KEY(movie_id, genre_id),
+    FOREIGN KEY(movie_id) REFERENCES movies(movie_id),
+    FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
 );
 
 
 COPY movies (movie_id, title, genres, imdb_id, tmdb_id, image_url) FROM '/docker-entrypoint-initdb.d/movies.csv' DELIMITER ',' CSV HEADER;
+COPY users (user_id) FROM '/docker-entrypoint-initdb.d/users.csv' DELIMITER ',' CSV HEADER;
 COPY ratings (user_id, movie_id, rating, timestamp) FROM '/docker-entrypoint-initdb.d/ratings.csv' DELIMITER ',' CSV HEADER;
 COPY tags (user_id, movie_id, tag, timestamp) FROM '/docker-entrypoint-initdb.d/tags.csv' DELIMITER ',' CSV HEADER;
+COPY genres (genre_id, genre) FROM '/docker-entrypoint-initdb.d/genres.csv' DELIMITER ',' CSV HEADER;
+COPY movies_genres (movie_id, genre_id) FROM '/docker-entrypoint-initdb.d/movies_genres.csv' DELIMITER ',' CSV HEADER;
