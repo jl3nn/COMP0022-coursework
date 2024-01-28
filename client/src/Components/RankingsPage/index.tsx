538a4166b740508a5b4f11d7f3f353b2561f39ce
jl3nn <jonas.lennie.21@ucl.ac.uk>
2024-01-28 19:54:52 +0000

    implementing new genre view

--- client/src/Components/RankingsPage/index.tsx
+++ client/src/Components/RankingsPage/index.tsx
@@ -21,15 +21,15 @@ function RankingsPage() {
     return (
         <Stack spacing={2} alignItems="center" margin={5}>
                 <Paper sx={{width:600}}>
-                    <Typography variant="h4">Most Popular Movies</Typography>
+                    <Typography variant="h4">Most Popular Genres</Typography>
                     <Box sx={{maxHeight: 'calc(50vh - 150px)', overflow:"auto",  width:600}}>
-                        <MovieCards data={popularMovies} />
+                        {popularMovies.map((m) => <Typography variant="h6">{m}</Typography>)}                    
                     </Box>
                 </Paper>
                 <Paper sx={{maxHeight: 'calc(50vh - 100px)', overflow:"auto",  width:600}}>
-                    <Typography variant="h4">Most Contrevertial Movies</Typography>
+                    <Typography variant="h4">Most Contrevertial Genres</Typography>
                     <Box sx={{maxHeight: 'calc(50vh - 150px)', overflow:"auto",  width:600}}>
-                        <MovieCards data={contrevertialMovies} />
+                        {contrevertialMovies.map((m) => <Typography variant="h6">{m}</Typography>)}
                     </Box>
                 </Paper>
         </Stack>
--- server/server.py
+++ server/server.py
@@ -20,7 +20,6 @@ def get_search_results():
         tags = data.get("tags", [])
         genres = data.get("genres", [])
         date = data.get("date", [])
-        # Mock data for testing purposes
         result_data = [
             {
                 "imageUrl": "https://resizing.flixster.com/dV1vfa4w_dB4wzk7A_VzThWUWw8=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzEyZDMyYjZmLThmNzAtNDliNC1hMjFmLTA2ZWY4M2UyMjJhMi5qcGc=",
@@ -41,14 +40,7 @@ def get_search_results():
 def get_popular_movies():
     try:
         result_data = [
-            {
-                "imageUrl": "https://resizing.flixster.com/dV1vfa4w_dB4wzk7A_VzThWUWw8=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzEyZDMyYjZmLThmNzAtNDliNC1hMjFmLTA2ZWY4M2UyMjJhMi5qcGc=",
-                "title": "Server is UP",
-                "rating": 5,
-                "genre": "Bar",
-                "tags": ["bla"],
-                "ratingsList": [1, 2, 3, 4, 5],
-            }
+            'Some Genre'
             for _ in range(10)
         ]
         return jsonify(result_data)
@@ -60,14 +52,7 @@ def get_popular_movies():
 def get_controvertial_movies():
     try:
         result_data = [
-            {
-                "imageUrl": "https://resizing.flixster.com/dV1vfa4w_dB4wzk7A_VzThWUWw8=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzEyZDMyYjZmLThmNzAtNDliNC1hMjFmLTA2ZWY4M2UyMjJhMi5qcGc=",
-                "title": "Server is UP",
-                "rating": 5,
-                "genre": "Bar",
-                "tags": ["bla"],
-                "ratingsList": [1, 2, 3, 4, 5],
-            }
+            'Some Other Genre'
             for _ in range(10)
         ]
         return jsonify(result_data)
