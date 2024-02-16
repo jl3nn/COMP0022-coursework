f82f4333efb07603a443f2be5896cab43c4d2b30
Sid Charaschanya <sidcharaschanya@gmail.com>
2024-02-16 09:37:16 +0000

    fix(server): add get_response() (#48)
    * fix(server): add get_response()
    
    * fix(server): minor fix
    
    * fix(server): cleanup
    
    * fix(server): add make_response()
    
    * fix(server): case

--- server/blueprints/common.py
+++ server/blueprints/common.py
@@ -1,11 +1,34 @@
-from flask import jsonify, Response
-from typing import Any, Callable
+from flask import jsonify, make_response, Response
+import psycopg
+from typing import Any, Callable, Optional
+
+CONN_INFO = {
+    "dbname": "comp0022",
+    "user": "admin",
+    "password": "top_secret_password",
+    "host": "database",
+    "port": "5432",
+}
 
 
-def json_response(results: list[tuple], func: Callable[[tuple], Any]) -> Response:
-    return jsonify(list(map(func, results)))
+def execute_query(query: str, params: Optional[dict]) -> list[tuple]:
+    with psycopg.connect(**CONN_INFO) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query, params)
+            results = cursor.fetchall()
+
+    conn.close()
+    return results
 
 
-def json_error(e: Exception) -> Response:
-    print(f"error: {str(e)}")
-    return jsonify({"error": str(e)}), 500
+def get_response(
+    query: str,
+    params: Optional[dict] = None,
+    func: Callable[[tuple], Any] = lambda row: row[0],
+) -> Response:
+    try:
+        results = execute_query(query, params)
+        return make_response(jsonify(list(map(func, results))), 200)
+    except Exception as error:
+        print(f"error: {str(error)}")
+        return make_response(jsonify({"error": str(error)}), 500)
--- server/blueprints/genres.py
+++ server/blueprints/genres.py
@@ -1,43 +1,37 @@
-from .common import json_response, json_error
-from .connector import execute_query
+from .common import get_response
 from flask import Blueprint, Response
 
 app = Blueprint("genres", __name__)
 
 
 def get_genres(agg_func: str, precision: int = 3) -> Response:
-    try:
-        results = execute_query(
-            f"""
-            SELECT
-                g.genre,
-                round({agg_func}(r.rating)::numeric, {precision}) AS stat
-            FROM
-                genres g
-            INNER JOIN
-                movies_genres mg ON g.genre_id = mg.genre_id
-            INNER JOIN
-                ratings r ON mg.movie_id = r.movie_id
-            GROUP BY
-                g.genre_id
-            ORDER BY
-                stat DESC
-            ;
-            """
-        )
-
-        # TODO: Modify client to include stat
-        # return json_response(results, lambda row: {"genre": row[0], "stat": row[1]})
-        return json_response(results, lambda row: row[0])
-    except Exception as e:
-        return json_error(e)
+    return get_response(
+        f"""
+        SELECT
+            g.genre,
+            ROUND({agg_func}(r.rating)::NUMERIC, {precision}) AS statistic
+        FROM
+            genres g
+        INNER JOIN
+            movies_genres mg ON g.genre_id = mg.genre_id
+        INNER JOIN
+            ratings r ON mg.movie_id = r.movie_id
+        GROUP BY
+            g.genre_id
+        ORDER BY
+            statistic DESC
+        ;
+        """,
+        # TODO: Modify client to include statistic
+        # func=lambda row: {"genre": row[0], "statistic": row[1]},
+    )
 
 
 @app.route("/popular", methods=["GET"])
 def get_popular_genres() -> Response:
-    return get_genres("avg")
+    return get_genres("AVG")
 
 
 @app.route("/controversial", methods=["GET"])
 def get_controversial_genres() -> Response:
-    return get_genres("stddev")
+    return get_genres("STDDEV")
