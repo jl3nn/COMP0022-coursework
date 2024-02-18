59bdbaeb7be5ea6168838ed9b09f56d3cc41dfe8
miberl <m.berlingieri02@gmail.com>
2024-02-18 12:17:02 +0000

    Added ratings history by genre

--- server/blueprints/genres.py
+++ server/blueprints/genres.py
@@ -1,5 +1,5 @@
 from .common import get_response
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 
 app = Blueprint("genres", __name__)
 
@@ -27,6 +27,43 @@ def get_genres(agg_func: str, precision: int = 3) -> Response:
     )
 
 
+def get_genres_by_user_preference(min_rating: int = 0, max_rating: int = 5) -> Response:
+    genre = request.args.get("genre")
+    return get_response(f"""
+        WITH UserLikedGenres AS (
+            SELECT
+                DISTINCT r.user_id,
+            FROM
+                ratings r
+            INNER JOIN
+                movies_genres mg ON r.movie_id = mg.movie_id
+            INNER JOIN
+                genres g ON mg.genre_id = g.genre_id
+            WHERE
+                g.genre = %(genre)s
+            GROUP BY
+                r.user_id
+            HAVING
+                AVG(r.rating) BETWEEN {min_rating} AND {max_rating}
+        ),
+
+        SELECT
+            g.genre, AVG(r.rating) as AvgRating
+        FROM
+            movies_genres mg
+        INNER JOIN
+            genres g ON mg.genre_id = g.genre_id
+        INNER JOIN
+            ratings r ON mg.movie_id = r.movie_id
+        INNER JOIN
+            RelevantUsers ru ON r.user_id = ru.user_id
+        GROUP BY
+            g.genre
+        ORDER BY
+            AvgRating DESC;
+        """, params={"genre": genre})
+
+
 @app.route("/popular", methods=["GET"])
 def get_popular_genres() -> Response:
     return get_genres("AVG")
@@ -35,3 +72,18 @@ def get_popular_genres() -> Response:
 @app.route("/controversial", methods=["GET"])
 def get_controversial_genres() -> Response:
     return get_genres("STDDEV")
+
+
+@app.route("/liked-genre", methods=["GET"])
+def get_user_liked_genres() -> Response:
+    return get_genres_by_user_preference(4, 5)
+
+
+@app.route("/disliked-genre", methods=["GET"])
+def get_user_disliked_genres() -> Response:
+    return get_genres_by_user_preference(0, 2)
+
+
+@app.route("/neutral-genre", methods=["GET"])
+def get_user_neutral_genres() -> Response:
+    return get_genres_by_user_preference(2, 4)
