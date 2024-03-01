e03abfddd5d655c0094671564ccd7c812abb2efd
miberl <m.berlingieri02@gmail.com>
2024-03-01 12:03:53 +0000

    Req. 5 UI updates

--- client/src/Components/NewFilmReport/index.tsx
+++ client/src/Components/NewFilmReport/index.tsx
@@ -1,64 +1,63 @@
-import { Box, Tab, Tabs, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import IndividualEstimate from './individual_estimate';
-import AccuracyEstimate from './accuracy_estimate';
+import { Box, Tab, Tabs } from "@mui/material";
+import React, { useState } from "react";
+import IndividualEstimate from "./individual_estimate";
+import AccuracyEstimate from "./accuracy_estimate";
 
 interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-  }
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
 
 function CustomTabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            {children}
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  function a11yProps(index: number) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const NewFilmReport = () => {
-    const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
-    function handleChange(event: React.SyntheticEvent, newValue: number) {
-        setValue(newValue);
-    }
+  function handleChange(event: React.SyntheticEvent, newValue: number) {
+    setValue(newValue);
+  }
 
-    return (
-        <Box>
-            <Typography>New Film Report</Typography>
-            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-                <Tab label="Individual Estimate" {...a11yProps(0)} />
-                <Tab label="Accuracy Estimate" {...a11yProps(1)} />
-            </Tabs>
+  return (
+    <Box>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <Tab label="Individual Prediction" {...a11yProps(0)} />
+        <Tab label="Accuracy Estimate" {...a11yProps(1)} />
+      </Tabs>
 
-            <CustomTabPanel value={value} index={0}>
-                <IndividualEstimate />
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={1}>
-                <AccuracyEstimate />
-            </CustomTabPanel>
-        </Box>
-    );
-}
+      <CustomTabPanel value={value} index={0}>
+        <IndividualEstimate />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <AccuracyEstimate />
+      </CustomTabPanel>
+    </Box>
+  );
+};
 
 export default NewFilmReport;
--- client/src/Components/NewFilmReport/individual_estimate.tsx
+++ client/src/Components/NewFilmReport/individual_estimate.tsx
@@ -1,6 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Box, List, ListItem, ListItemText, Paper, Stack, Typography } from '@mui/material';
-import AutocompleteWithFetch from '../common/AutocompleteSelector';
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Collapse,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Stack,
+} from "@mui/material";
+import AutocompleteWithFetch from "../common/AutocompleteSelector";
 
 interface Rating {
   averageBias: number;
@@ -16,106 +28,152 @@ function IndividualEstimate() {
   const [rating, setRating] = useState<Rating | null>(null);
   const [movie, setMovie] = useState<String | null>(null);
   const [user, setUser] = useState<String[]>([]);
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   useEffect(() => {
     const calculateRating = async () => {
+      setLoading(true);
       try {
-        const users_response = await fetch('http://localhost:5555/users/for-prediction', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ movie: movie }),
-        });
+        const users_response = await fetch(
+          "http://localhost:5555/users/for-prediction",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ movie: movie }),
+          }
+        );
 
         const users = await users_response.json();
         setUser(users);
 
-        const response = await fetch('http://localhost:5555/ratings/prediction', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ users: users, movie: movie }),
-        });
+        const response = await fetch(
+          "http://localhost:5555/ratings/prediction",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ users: users, movie: movie }),
+          }
+        );
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
 
         const rating = await response.json();
         setRating(rating);
       } catch (error: any) {
-        console.error('Error calculating skew:', error.message);
+        console.error("Error calculating skew:", error.message);
       }
+      setLoading(false);
     };
 
     if (movie) {
-      console.log(":)")
       calculateRating();
     } else {
-      console.log(":(")
       setRating(null);
     }
   }, [movie]);
 
+  const errorCalculation = rating
+    ? Math.abs(rating.averageRating - rating.predictedRating).toFixed(2)
+    : 0;
+
   return (
-    <Stack spacing={2} alignItems="center" maxWidth={800} margin='auto'>
+    <Stack spacing={2} alignItems="center" maxWidth={800} margin="auto">
       <AutocompleteWithFetch
         value={movie}
         label="Movie"
         apiUrl="http://localhost:5555/autocomplete/movie"
         onChange={(_: any, newValue: any) => setMovie(newValue)}
       />
-
-      <Typography>
-        {rating ? (
-          <Box sx={{ p: 2, bgcolor: 'background.default', borderRadius: 2 }}>
-            <Typography variant="h6" component="h2" gutterBottom>
-              Movie Analysis
+      {rating && !loading && (
+        <Card sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ fontSize: "1.5rem" }}
+            >
+              {movie}
+            </Typography>
+            <Typography
+              sx={{ mb: 1.5, fontSize: "1.25rem" }}
+              color="text.secondary"
+            >
+              Rating Summary
             </Typography>
             <Typography variant="subtitle1" gutterBottom>
-              For the movie: <strong>{movie}</strong>
+              Analyzed for users: <strong>{user.join(", ")}</strong>
             </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-              Analyzed for users: <strong>{user.join(', ')}</strong>
+            <Typography variant="body2" sx={{ fontSize: "1rem" }}>
+              Simple Prediction: {rating.subsetRating.toFixed(2)}
+              <br />
+              Bias-Adjusted Prediction: {rating.predictedRating.toFixed(2)}
+              <br />
+              <strong>Actual Rating: {rating.averageRating.toFixed(2)}</strong>
+              <br />
+              <Box sx={{ mt: 2, fontSize: "1rem", color: "error.main" }}>
+                Prediction Error: {errorCalculation}
+              </Box>
             </Typography>
-            <Paper elevation={3} sx={{ mt: 2, mb: 2, p: 2 }}>
-              <Typography variant="body1" component="div">
-                The following estimates have been produced:
+          </CardContent>
+          <CardActions disableSpacing>
+            <Button
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              Bias Analysis
+            </Button>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph sx={{ fontSize: "1.25rem" }}>
+                Bias Analysis:
               </Typography>
               <List dense>
                 <ListItem>
-                  <ListItemText primary={`Average Bias: ${rating.averageBias}`} />
+                  <ListItemText
+                    primary={`Average Bias: ${rating.averageBias}`}
+                  />
                 </ListItem>
                 <ListItem>
-                  <ListItemText primary={`Average Rating: ${rating.averageRating}`} />
+                  <ListItemText
+                    primary={`Genre Bias: ${rating.genreBias}`}
+                    sx={{ fontSize: "1.25rem" }}
+                  />
                 </ListItem>
                 <ListItem>
-                  <ListItemText primary={`Genre Bias: ${rating.genreBias}`} />
+                  <ListItemText
+                    primary={`Tag Bias: ${rating.tagBias}`}
+                    sx={{ fontSize: "1.25rem" }}
+                  />
                 </ListItem>
                 <ListItem>
-                  <ListItemText primary={`Predicted Rating: ${rating.predictedRating}`} />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary={`Subset Rating: ${rating.subsetRating}`} />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary={`Tag Bias: ${rating.tagBias}`} />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary={`User Bias: ${rating.userBias}`} />
+                  <ListItemText
+                    primary={`User Bias: ${rating.userBias}`}
+                    sx={{ fontSize: "1.25rem" }}
+                  />
                 </ListItem>
               </List>
-            </Paper>
-            <Typography variant="body2" color="textSecondary" align="right">
-              Data provided is based on current analysis metrics.
-            </Typography>
-          </Box>          
-        ) : (
-          <Typography>Please select some ratings or tags</Typography>
-        )}
-      </Typography>
+              {/* Visualization Component here if applicable */}
+            </CardContent>
+          </Collapse>
+        </Card>
+      )}
+      {!rating && !loading && (
+        <Typography>Please select a movie to see predictions.</Typography>
+      )}
+      {loading && <Typography>Loading...</Typography>}
     </Stack>
   );
 }
